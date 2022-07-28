351625e03e8e1afd4e6be35749796f338d6c6c84
Ian Hartley <Ian.Hartley@tfg-brands.co.uk>
2022-07-28 14:24:21 +0100

    initial commit

---
+++ calc.py
@@ -0,0 +1,14 @@
+#goto 4
+#replace 1 2
+#remove 3
+#goto 2
+#got calc x 3 5 
+#replace 6 10
+
+
+
+
+
+Calc =[]
+Calc = input( "enter calculation X + - or/ followed by 2 integers separated by spaces ")
+print (Calc)
